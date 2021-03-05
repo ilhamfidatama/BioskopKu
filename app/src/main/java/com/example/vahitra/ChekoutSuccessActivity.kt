c08757455828bf3a75aab1b7ebcd19b5b2ec0f85
haidarrahman30 <haidarrahmans30@gmail.com>
2021-03-05 15:50:07 +0800

    push project bioskopku v1

---
+++ app/src/main/java/com/example/vahitra/ChekoutSuccessActivity.kt
@@ -0,0 +1,11 @@
+package com.example.vahitra
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChekoutSuccessActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chekout_success)
+    }
+}
